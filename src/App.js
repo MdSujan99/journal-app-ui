2d347591aa4b6a99b0590458c5b0bc33a809e095
MdSujan99 <khans8962@gmail.com>
2024-01-02 00:43:26 +0530

    2. activate our router and load the route definitions we defined in the first step 3. to make sure we have all the components we wanna load and add some means of navigation for the routes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./component/pages/Home";
 import AboutPage from "./component/pages/About";
 import ContactPage from "./component/pages/Contact";
+import RootLayout from "./component/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> }, // route 1
-  { path: "/about", element: <AboutPage /> }, // route 2
-  { path: "/contact", element: <ContactPage /> }, // route 3
+  {
+    path: "/", // root layout
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> }, // route 1
+      { path: "/about", element: <AboutPage /> }, // route 2
+      { path: "/contact", element: <ContactPage /> }, // route 3
+    ],
+  },
 ]);
 
 function App() {
-  // const [journals, setJournals] = useState([]);
-
-  // const getJournals = async () => {
-  //   try {
-  //     const response = await api.get("/journal");
-  //     setJournals(response.data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getJournals();
-  // }, []);
-
   return <RouterProvider router={router} />;
 }
 
